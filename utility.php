6b5cfc3ec4c921ddb0554f93d6d5d635057d69b6
Mykola Ostrovskyy <ostrovsm@gmail.com>
2010-06-19 16:41:40 +0200

    + gcd()

--- utility.php
+++ utility.php
@@ -1,6 +1,7 @@
 <?php
 
 /***************************************************************************
+    Returns character offset of the first mismatch between two strings
 ***************************************************************************/
 function strdiff( $string1, $string2 )
 {
@@ -15,4 +16,31 @@ function strdiff( $string1, $string2 )
     }
 
     return $i;
+}
+
+/***************************************************************************
+    Returns gretest common divider of two positive integers
+***************************************************************************/
+function gcd( $number1, $number2 )
+{
+    $number1 = intval( $number1 );
+    $number2 = intval( $number2 );
+
+    if ( ( $number1 > 0 ) && ( $number2 > 0 ) )
+    {
+        $reminder = $number1 % $number2;
+
+        while ( $reminder != 0 )
+        {
+            $number1  = $number2;
+            $number2  = $reminder;
+            $reminder = $number1 % $number2;
+        }
+    }
+    else
+    {
+        $number2 = 0;
+    }
+
+    return $number2;
 }
