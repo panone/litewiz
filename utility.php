3b38a7b61ff60884a59b4966e161cc16da1f4c67
Mykola Ostrovskyy <ostrovsm@gmail.com>
2010-06-21 19:17:10 +0200

    + integer factorization

--- utility.php
+++ utility.php
@@ -44,3 +44,82 @@ function gcd( $number1, $number2 )
 
     return $number2;
 }
+
+/***************************************************************************
+    Returns array of prime factors of an integer
+***************************************************************************/
+function primefact( $number )
+{
+    $result = array();
+    $factor = 2;
+
+    while ( ( $factor * $factor ) <= $number )
+    {
+        if ( ( $number % $factor ) == 0 )
+        {
+            $result[] = $factor;
+            $number  /= $factor;
+        }
+        else
+        {
+            $factor++;
+        }
+    }
+
+    $result[] = $number;
+
+    return $result;
+}
+
+/***************************************************************************
+    Returns array of all possible factor pairs of an integer
+***************************************************************************/
+function pairfact( $number )
+{
+    $prime  = primefact( $number );
+    $primes = count( $prime );
+    $setSize1 = 1;
+    $setSize2 = $primes - $setSize1;
+    $factor   = array( 1 => $number );
+
+    if ( $primes == 2 )
+    {
+        $factor[ $prime[ 0 ] ] = $prime[ 1 ];
+    }
+
+    while ( $setSize1 <= $setSize2 )
+    {
+        for ( $i = 0; $i < ( $primes - ( $setSize1 - 1 ) ); $i++ )
+        {
+            $factor1 = 1;
+            $factor2 = 1;
+
+            for ( $j = 0; $j < $setSize1; $j++ )
+            {
+                $factor1 *= $prime[ $i + $j ];
+            }
+
+            for ( $k = 0; $k < $i; $k++ )
+            {
+                $factor2 *= $prime[ $k ];
+            }
+
+            for ( $k = $i + $setSize1; $k < $primes; $k++ )
+            {
+                $factor2 *= $prime[ $k ];
+            }
+
+            $factor[ min( $factor1, $factor2 ) ] = max( $factor1, $factor2 );
+        }
+
+        $setSize1++;
+        $setSize2 = $primes - $setSize1;
+    }
+
+    foreach ( $factor as $factor1 => $factor2 )
+    {
+        $result[] = array( $factor1, $factor2 );
+    }
+
+    return $result;
+}
